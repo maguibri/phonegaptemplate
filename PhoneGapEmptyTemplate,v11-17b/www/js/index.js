16c602eb754bf5f03168d2ffeed3fdc74d776462
brianmaguire39@gmail.com <brianmaguire39@gmail.com>
2018-08-29 21:11:30 -0400

    sec

---
+++ PhoneGapEmptyTemplate,v11-17b/www/js/index.js
@@ -0,0 +1,23 @@
+/*
+ * index.js
+ * Put your JavaScript in here
+ */
+
+"use strict;"
+
+/*===========================*/
+/* put global variables here */
+/*===========================*/
+
+
+/* wait until all phonegap/cordova is loaded then call onDeviceReady*/
+document.addEventListener("deviceready", onDeviceReady, false);
+
+function onDeviceReady(){
+	alert("onDeviceReady() fired");
+}
+
+/*====================*/
+/* put functions here */
+/*====================*/
+
